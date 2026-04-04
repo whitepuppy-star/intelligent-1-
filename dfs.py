fb4288efca7273a25650641c428cf442665986d6
whitepuppy-star <sehwasong27@gmail.com>
2026-04-04 18:05:27 +0900

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,37 +1,32 @@
 def depth_first_search(problem):
-    # 시작 상태와 빈 경로를 스택에 저장. 스택 구조 : (현재 상태, 여기까지 오기위한 행동 리스트)
-    StartNode = (problem.getStartState(), [])
-    stack = [StartNode]
+    # 1. 초기 설정 
+    StartNode = problem.getStartState()
+    # 방문한 상태를 기록할 셋
+    Visited = set() 
+    # 탐색할 노드를 담은 스택 (현재 상태, 그 상태까지의 경로)
+    Stack = [(StartNode, [])] 
+    
+    ExpandedNodeCount = 0  # 확장한 노드 수 기록용 
 
-    # 방문한 상태를 기록할 set을 생성. (그래프 탐색이니까)
-    explored = set()
+    while Stack:
+        # 2. 스택에서 하나를 꺼냄. NextState는 CurrentState로. 경로리스트는 Actions 변수로.
+        CurrentState, Actions = Stack.pop() 
 
-    expandedNode = 0
+        # 3. 목표 상태인지 확인. 목표라면 (경로, 경로 길이, 확장 노드 수) 반환.
+        if problem.isGoalState(CurrentState): 
+            return Actions, len(Actions), ExpandedNodeCount 
 
-    while stack:
-        # 스택의 가장 마지막 노드 꺼내기
-        CurrentState, actions = stack.pop()
+        # 4. 방문 여부 체크 
+        if CurrentState not in Visited: 
+            Visited.add(CurrentState) 
+            ExpandedNodeCount += 1 
 
-        # 목표 상태에 도달했는지 확인하기
-        if problem.isGoalState(CurrentState):
-            return actions, len(actions), len(explored)
-        
-        # 아직 방문하지 않은 경우멘 자식 노드를 확장
-        if CurrentState not in explored:
-            explored.add(CurrentState)
-            expandedNode += 1
+            # 5. 주변 노드(자식) 탐색
+            for NextState, Action, Cost in problem.getSuccessors(CurrentState):
+                # 6. 주변 노드가 아직 탐색 전이라면 스택에 추가
+                if NextState not in Visited:
+                    Stack.append((NextState, Actions + [Action])) # 자식 노드를 스택에 튜플로 추가.
 
-            # 현재 상태에서 갈 수 있는 다음 노드들을 가져오기
-            # successors 리스트 (다음 상태, 취한 행동, 이동 비용)
-            successors = problem.getSuccessors(CurrentState)
-
-            for NextState, action, cost in successors:
-                if NextState not in explored:
-                    # 새로운 경로를 생성하여 스택에 추가
-                    # 기존 actions 리스트에 현재의 action을 추가
-                    NewAction = actions + [action]
-                    stack.append((NextState, NewAction))
-
-    # 탐색 실패한 경우, 빈 리스트 반환
-    return [], 0, expandedNode
+    # 탐색 실패 시 None이 아닌 빈 값들을 반환 (에러 방지)
+    return [], 0, ExpandedNodeCount 
 
